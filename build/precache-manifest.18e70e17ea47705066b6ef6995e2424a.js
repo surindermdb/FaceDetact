80b8a95a206f60b97e6acf95a9aa0fbc1e6497c2
surindermdb <surinderkumar.mdb@gmail.com>
2022-07-01 16:16:40 +0530

    first commit

---
+++ build/precache-manifest.18e70e17ea47705066b6ef6995e2424a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c7f3124ce844d9abff57785422603b2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1be810df842e603ed9e4",
+    "url": "/static/css/main.523f09c5.chunk.css"
+  },
+  {
+    "revision": "c0fee82ea8142d35642e",
+    "url": "/static/js/2.376fbb1f.chunk.js"
+  },
+  {
+    "revision": "b183437b48fb0c7c10973acfa520fbc1",
+    "url": "/static/js/2.376fbb1f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1be810df842e603ed9e4",
+    "url": "/static/js/main.8becdb96.chunk.js"
+  },
+  {
+    "revision": "9030c7668e656f2f612a",
+    "url": "/static/js/runtime-main.6c5be910.js"
+  }
+]);
